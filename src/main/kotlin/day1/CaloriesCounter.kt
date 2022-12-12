51614cef4ceaa281173a5fde0579cd9b73465ded
alexander.otto <alexander.otto@check24.de>
2022-12-12 14:24:03 +0100

    day1 refactoring

--- src/main/kotlin/day1/CaloriesCounter.kt
+++ src/main/kotlin/day1/CaloriesCounter.kt
@@ -7,25 +7,23 @@ class CaloriesCounter {
 
     private val calories = File("/Users/alexander.otto/AdventOfCoding/advent-of-coding/src/main/resources/testData").readLines()
 
-    private var actualIndex = 0
-
     fun count(): Int {
         val accumulator = mutableListOf<Int>()
         accumulator.add(0, 0)
-        val c = calories.fold(accumulator) { acc, s ->
+        val caloriesPerElf = calories.fold(accumulator) { acc, s ->
             if (s.isEmpty()) {
-                actualIndex++
-                acc.add(actualIndex, 0)
+                acc.add(acc.size, 0)
                 acc
             } else {
-                acc[actualIndex] += try {
+                acc[acc.size - 1] += try {
                     Integer.parseInt(s)
-                } catch (e: Exception) { 0 }
+                } catch (e: Exception) {
+                    0
+                }
                 acc
             }
         }
-        val result = c.max()
 
-        return result
+        return caloriesPerElf.max()
     }
 }
