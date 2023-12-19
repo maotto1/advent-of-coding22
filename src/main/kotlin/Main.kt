60f69d5a85d80c6382635c5bac28e7adbbddcbf0
alexander.otto <alexander.otto@check24.de>
2023-12-19 18:25:21 +0100

    read test file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
-import day1.CaloriesCounter
+import java.io.File
+import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     println("Hello World!")
@@ -7,7 +8,13 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-    val result = CaloriesCounter().count()
+    val result = readInput("testData")
 
     println("$result")
+}
+
+fun readInput(fileName: String) : List<String> {
+    val path = Paths.get("").toAbsolutePath().toString()
+    val internalPath = "src/main/resources"
+    return File("$path/$internalPath/$fileName").readLines()
 }
